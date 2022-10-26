aa34264f1c4e4010f53ef03dc7e10f4e5e6649ca
Mason Tillie <masvertil58.dd@gmail.com>
2022-10-26 02:30:56 -0400

    commit

---
+++ main.js
@@ -0,0 +1,22 @@
+// Exercise 1
+function findWords(dogNames, dogString){
+    let dogNames =["Max", "HAS", "PuRple", 'dog']
+     
+}
+console.log(dogNames)
+
+
+
+
+
+
+// Exercise 2
+
+var names = ["Max","Baseball","Reboot","Goku","Trucks","Rodger"]
+
+console.log(names.splice(0, 1, "Even Index"))
+console.log(names.splice(2, 1, "Even Index"))
+console.log(names.splice(3, 1, "Even Index"))
+console.log(names.splice(4, 1, "Even Index"))
+
+console.log(names)
